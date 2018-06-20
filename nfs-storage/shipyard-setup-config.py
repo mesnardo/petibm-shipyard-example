0957d08c19ee88d67d94d81303074e5489583ec6
mesnardo <mesnardo@gwu.edu>
2018-06-20 19:47:10 -0400

    Minor change in generic credentials config file

--- nfs-storage/shipyard-setup-config.py
+++ nfs-storage/shipyard-setup-config.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 """
 Setup the YAML configuration files to be used with Batch Shipyard.
 """
 
-import pathlib
+import os
 import argparse
 import subprocess
+import ast
 
 
 def parse_command_line():
@@ -35,16 +37,86 @@ def parse_command_line():
 
 
 def get_storage_account_key(resource_group, account_name):
-  cmd_str = ('az storage account keys list'
+  cmd_str = ('az storage account keys list '
              '--resource-group {} --account-name {} '
              '--output json --query "[0].value"'
              .format(resource_group, account_name))
   output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
-  return output
+  return ast.literal_eval(output)
+
+
+def get_resource_group_location(resource_group):
+  cmd_str = ('az group show '
+             '--resource-group {} '
+             '--output json --query "location"'
+             .format(resource_group))
+  output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
+  return ast.literal_eval(output)
+
+
+def get_subscription_ids():
+  cmd_str = ('az account show --output json --query "[id, tenantId]"')
+  output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
+  return ast.literal_eval(output)
+
+
+def get_username():
+  cmd_str = ('az account show --output json --query "user.name"')
+  output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
+  return ast.literal_eval(output)
+
+
+def get_batch_account_name(resource_group):
+  cmd_str = ('az batch account list '
+             '--resource-group {} '
+             '--output json --query "[0].name"'
+             .format(resource_group))
+  output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
+  return ast.literal_eval(output)
+
+
+def get_batch_account_endpoint(resource_group):
+  cmd_str = ('az batch account list '
+             '--resource-group {} '
+             '--output json --query "[0].accountEndpoint"'
+             .format(resource_group))
+  output = subprocess.check_output(cmd_str, shell=True).decode('ascii')
+  return ast.literal_eval(output)
+
+
+def get_config_paths(config_dir):
+  if os.path.isdir(config_dir):
+    filenames = ['config.yaml', 'credentials.yaml', 'fs.yaml',
+                 'pool.yaml', 'jobs.yaml']
+    return [os.path.join(config_dir, name) for name in filenames]
+
+
+def replace_strings_in_files(filepaths, info):
+  for path in filepaths:
+    with open(path, 'r') as infile:
+      lines = infile.readlines()
+    for i, line in enumerate(lines):
+      for key, value in info.items():
+        if key in line:
+          lines[i] = line.replace(key, value)
+    with open(path, 'w') as outfile:
+      outfile.writelines(lines)
 
 
 def main(args):
-  data = {}
+  info = {}
+  info['<resource-group>'] = args.resource_group
+  info['<storage-account-name>'] = args.account_name
+  info['<storage-account-key>'] = get_storage_account_key(args.resource_group,
+                                                          args.account_name)
+  info['<location>'] = get_resource_group_location(args.resource_group)
+  info['<subscription-id>'], info['<tenant-id>'] = get_subscription_ids()
+  info['<username>'] = get_username()
+  info['<batch-account-name>'] = get_batch_account_name(args.resource_group)
+  url = 'https://' + get_batch_account_endpoint(args.resource_group)
+  info['<batch-account-service-url>'] = url
+  configpaths = get_config_paths(args.config_dir)
+  replace_strings_in_files(configpaths, info)
 
 
 if __name__ == '__main__':
